5931a06d8d62961648265d4e0ba3bbfe3f06cdb1
JeffersonCabello <jefcsanc@espol.edu.ec>
2023-05-23 07:03:25 -0500

    Programa sensor de peso HX711

--- ProgramaESP32.ino
+++ ProgramaESP32.ino
@@ -1,7 +1,22 @@
+#include <Arduino.h>
+#include "HX711.h"
+#include "soc/rtc.h"
+#include "BluetoothSerial.h"
+#include "esp_adc_cal.h"
+#include <Preferences.h>
+
+// Pines para el sensor de peso HX711
+const int LOADCELL_DOUT_PIN = 16;
+const int LOADCELL_SCK_PIN = 4;
+
+HX711 scale;
+Preferences preferences;
+
+// Pines para el sensor ultrasonico
 const int trigPin = 5;
 const int echoPin = 18;
 
-//define sound speed in cm/uS
+//Velocidad del sonido en cm/uS
 #define SOUND_SPEED 0.034
 
 long duration;
@@ -11,9 +26,20 @@ void setup() {
   Serial.begin(115200); // Starts the serial communication
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+
+  preferences.begin("NPC-Balanzas", false); 
+  double calibracion = preferences.getDouble("calibracion",14.83);
+  scale.set_scale(14.83);
+  scale.tare(); // reset the scale to 0
+  preferences.end();
+  
 }
 
 void loop() {
+
+  double oneReading = (scale.get_units(5)/1000);
+
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -29,8 +55,11 @@ void loop() {
   distanceCm = duration * SOUND_SPEED/2;
 
   // Prints the distance in the Serial Monitor
-  Serial.print("Distance (cm): ");
+  Serial.print("Distancia (cm): ");
   Serial.println(distanceCm);
+
+  Serial.print("Peso (Kg): ");
+  Serial.println(oneReading);
   
   delay(1000);
 }
